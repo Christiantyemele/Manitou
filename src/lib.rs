90001b1de9e20d18c89cff7a2cef648954d0ab4d
Christiantyemele <yemelechristian2@gmail.com>
2024-08-18 16:04:31 +0100

    feat(): init step 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,63 @@
-use std::fmt::{self, Display};
+use std::path::PathBuf;
+use std::sync::mpsc;
 
-mod compat;
-#[macro_use]
-pub mod constant;
-pub struct EnumParseError(());
+use evdev::uinput::VirtualDeviceBuilder;
+use evdev::{AttributeSet, EventType, InputEvent, RelativeAxisType};
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
+use tokio::fs::File;
+use tokio::io::{self, Result};
+use tokio::net::TcpStream;
+use tokio::spawn;
+use tokio::sync::watch::{self, Receiver, Sender};
 
-impl Display for EnumParseError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "failed to parse Key from string")
-    }
+pub async fn handle_device_request(stream: TcpStream) -> Result<()> {
+    // spilt the stream into reader and writer
+    let mut stream = stream;
+    let (_rd, mut wr) = stream.split();
+
+    // sample
+    let inputevent = InputEvent::new(EventType::UINPUT, 0xff67, 0);
+    let (tx, rx): (Sender<InputEvent>, Receiver<InputEvent>) = watch::channel(inputevent);
+
+    // Create virtual device on client
+    let mut device = VirtualDeviceBuilder::new()
+        .unwrap()
+        .name("mouse")
+        .with_relative_axes(&AttributeSet::from_iter([
+            RelativeAxisType::REL_X,
+            RelativeAxisType::REL_Y,
+            RelativeAxisType::REL_WHEEL,
+            RelativeAxisType::REL_HWHEEL,
+        ]))
+        .unwrap()
+        .build()
+        .unwrap();
+
+    // move to syspath of device file in /dev/input/?
+    spawn(async move {
+        loop {
+            for path in device.enumerate_dev_nodes_blocking().unwrap() {
+                let path = path.unwrap();
+
+                let mut watcher = RecommendedWatcher::new(tx, Config::default()).unwrap();
+                watcher.watch(&path, RecursiveMode::Recursive).unwrap();
+            }
+        }
+    });
+    // creating a task which will continuously check for new events in a non-blocking fashion
+    spawn(async move {
+        loop {
+            for ev in rx {}
+        }
+    });
+
+    // collect binary events from server, convert them to `hexadecimal code`, `bringout the i32 value`, `bring out the u16 type`
+
+    // call th new method with the three inputs then emit them to the virtual device created on the client
+    let mut _opened_file = File::open("/dev/input/event16").await?;
+
+    // asynchronoulsy writing to writer in real time
+    io::copy(&mut _opened_file, &mut wr).await?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use evdev::uinput::{VirtualDevice, VirtualDeviceBuilder};
-use evdev::AttributeSet;
+use manitou::handle_device_request;
 use std::borrow::Borrow;
-use tokio::fs::File;
-use tokio::io::{self, Result};
-use tokio::net::{TcpListener, TcpStream};
+use tokio::io::Result;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // collecting the ip address for the communication as first passed argument
     let mut address = std::env::args().nth(1).expect("expects a valid IP address");
-
+    let mode = std::env::args()
+        .nth(2)
+        .expect("second argument should specify if you are server or client e.g server or client");
     address.push_str(":6935").borrow();
     // todo give permissions to file and try reading in real time
 
@@ -26,23 +26,4 @@ async fn main() -> Result<()> {
         });
         ()
     }
-}
-async fn handle_device_request(stream: TcpStream) -> Result<()> {
-    // spilt the stream into reader and writer
-    let mut stream = stream;
-    let (_rd, mut wr) = stream.split();
-
-    // create a virtual device on client
-    // let mut device = VirtualDeviceBuilder::new().unwrap()
-    // .name("mouse")
-    // .with_relative_axes(&AttributeSet::from_iter([
-    //     RelativeAxisCode::REL_X
-    // ])).unwrap()
-
-    let mut _opened_file = File::open("/dev/input/event16").await?;
-
-    // asynchronoulsy writing to writer in real time
-    io::copy(&mut _opened_file, &mut wr).await?;
-
-    Ok(())
 }
